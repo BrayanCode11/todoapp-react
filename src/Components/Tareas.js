3559350e54cbb1a341f671c1fde07f96ae55d063
BrayanCode11 <bangarita@unal.edu.co>
2023-12-31 15:32:40 -0500

    Add close icon to task component

--- src/Components/Tareas.js
+++ src/Components/Tareas.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "../styles/Tarea.css"
+import "../styles/Tarea.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
 
 function Tareas({ texto }) {
   return (
     <div className="tarea-contenedor">
       <div className="tarea-texto">{texto}</div>
-      <div className="tarea-icono">Elimminar</div>
+      <div className="tarea-icono">
+        <AiOutlineCloseCircle className="tarea-icono"/>
+      </div>
     </div>
   );
 }
