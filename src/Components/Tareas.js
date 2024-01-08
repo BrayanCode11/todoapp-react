ab596cdb3f53585bd69d965bd42ab6f080cad273
BrayanCode11 <bangarita@unal.edu.co>
2024-01-07 21:15:23 -0500

    Add completion styling to Tareas component

--- src/Components/Tareas.js
+++ src/Components/Tareas.js
@@ -2,12 +2,16 @@ import React from "react";
 import "../styles/Tarea.css";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
-function Tareas({ texto }) {
+function Tareas({ texto, completada }) {
   return (
-    <div className="tarea-contenedor">
+    <div
+      className={
+        completada ? "tarea-contenedor completada" : "tarea-contenedor"
+      }
+    >
       <div className="tarea-texto">{texto}</div>
       <div className="tarea-icono">
-        <AiOutlineCloseCircle className="tarea-icono"/>
+        <AiOutlineCloseCircle className="tarea-icono" />
       </div>
     </div>
   );
